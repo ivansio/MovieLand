326d6dfddf78adc87f0c965f4cd56ce78ad49a38
Ivan <_ivan_@bigmir.net>
2016-06-13 12:03:36 +0300

    add

--- src/main/java/com/luxoft/movieLand/dao/jdbc/JdbcMovieDao.java
+++ src/main/java/com/luxoft/movieLand/dao/jdbc/JdbcMovieDao.java
@@ -1,14 +1,12 @@
 package com.luxoft.movieLand.dao.jdbc;
 
 import com.luxoft.movieLand.dao.MovieDao;
+import com.luxoft.movieLand.dao.jdbc.mapper.CountryRowMapper;
+import com.luxoft.movieLand.dao.jdbc.mapper.GenreRowMapper;
 import com.luxoft.movieLand.dao.jdbc.mapper.MovieRowMapper;
 import com.luxoft.movieLand.entity.Movie;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowCallbackHandler;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +19,7 @@ import javax.sql.DataSource;
  * Created by red5 on 08.06.2016.
  */
 @Service
-public class JdbcMovieDao implements MovieDao{
+public class JdbcMovieDao implements MovieDao {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
@@ -30,6 +28,13 @@ public class JdbcMovieDao implements MovieDao{
 
     @Autowired
     private String getMovieByIdSQL;
+
+    @Autowired
+    private String getRefMovieCountrySQL;
+
+    @Autowired
+    private String getRefMovieGenreSQL;
+
     @Autowired
     private DataSource dataSource;
 
@@ -37,6 +42,8 @@ public class JdbcMovieDao implements MovieDao{
     @Override
     public Movie getById(int id) {
         Movie movie = jdbcTemplate.queryForObject(getMovieByIdSQL, new Object[]{id},new MovieRowMapper());
+        movie.setCountryList(jdbcTemplate.query(getRefMovieCountrySQL, new Object[]{id},new CountryRowMapper()));
+        movie.setGenreList(jdbcTemplate.query(getRefMovieGenreSQL, new Object[]{id},new GenreRowMapper()));
         return movie;
     }
 }
